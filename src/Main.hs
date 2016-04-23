86e258497478f95ded5d8a04cede601d82b5a0e1
Dennis Vriend <dnvriend@gmail.com>
2016-04-23 18:09:19 +0200

    added types

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
 import Data.Hex
 
+question :: String
 question = "Type a string to convert to hexadecimal"
-response inpStr = "The hexadecimal representation of " ++ inpStr ++ " is: " ++ hex inpStr
+
+response :: String -> String -> String
+response inpStr hexStr = "The hexadecimal representation of " ++ inpStr ++ " is: " ++ hexStr
 
 main :: IO ()
 main = do
        putStrLn question
        inpStr <- getLine
-       putStrLn $ response inpStr
+       putStrLn $ response inpStr $ hex inpStr
